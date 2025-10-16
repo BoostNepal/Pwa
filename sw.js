bfeb8e7d0b3eaabac898fab23e5b44a8e49f842c
BoostNepal <officialboostnepal@gmail.com>
2025-10-16 12:25:03 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,36 @@
 const CACHE_NAME = 'passing-fire-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/images/icon-512.png'
+    './',
+    './index.html',
+    './images/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
+            .then((cache) => {
+                console.log('Caching files');
+                return cache.addAll(urlsToCache);
+            })
+            .catch((error) => {
+                console.error('Caching failed:', error);
+            })
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => response || fetch(event.request))
+            .then((response) => {
+                if (response) {
+                    console.log('Serving from cache:', event.request.url);
+                    return response;
+                }
+                console.log('Fetching from network:', event.request.url);
+                return fetch(event.request);
+            })
+            .catch((error) => {
+                console.error('Fetch failed:', error);
+            })
     );
 });
